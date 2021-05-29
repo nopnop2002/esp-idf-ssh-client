631589d9e5e84a13c9929056c6021c28e1d89244
nopnop2002 <hau26280@tree.odn.ne.jp>
2021-05-29 11:02:56 +0900

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # esp-idf-ssh-client
-ssh client for esp-idf
+ssh client for esp-idf.   
+This project use [this](https://github.com/libssh2/libssh2) ssh library.   
+
+# Software requirements
+esp-idf v4.4 or later.   
+
+# Installation for ESP32
+
+```
+git clone https://github.com/nopnop2002/esp-idf-ssh-client
+cd esp-idf-ssh-client/
+git clone https://github.com/libssh2/libssh2 components/libssh2
+cp esp-idf/libssh2_config.h components/libssh2/include
+cp esp-idf/CMakeLists.txt components/libssh2
+idf.py set-target esp32
+idf.py menuconfig
+idf.py flash
+```
+
+# Installation for ESP32-S2
+
+```
+git clone https://github.com/nopnop2002/esp-idf-ssh-client
+cd esp-idf-ssh-client/
+git clone https://github.com/libssh2/libssh2 components/libssh2
+cp esp-idf/libssh2_config.h components/libssh2/include
+cp esp-idf/CMakeLists.txt components/libssh2
+idf.py set-target esp32s2
+idf.py menuconfig
+idf.py flash
+```
+
+# Configuration   
+You have to set this config value with menuconfig.   
+- CONFIG_ESP_WIFI_SSID   
+SSID (network name) to connect to.
+- CONFIG_ESP_WIFI_PASSWORD   
+WiFi password (WPA or WPA2) to use.
+- CONFIG_ESP_MAXIMUM_RETRY   
+Set the Maximum retry to avoid station reconnecting to the AP unlimited when the AP is really inexistent.
+- CONFIG_SSH_USER   
+Username of SSH.
+- CONFIG_SSH_PASSWORD   
+Password of SSH.
+- CONFIG_SSH_HOST   
+IP address of SSH host.   
+__mDMS name cannot be used.__   
+- CONFIG_SSH_COMMAND1   
+Command to execute.   
+- CONFIG_SSH_COMMAND2   
+Command to execute.   
+
+![config-main](https://user-images.githubusercontent.com/6020549/120054821-3d755500-c06d-11eb-950c-d357d0a9fdef.jpg)
+![config-app](https://user-images.githubusercontent.com/6020549/120054822-3fd7af00-c06d-11eb-8e0a-2cfbed407ad1.jpg)
+
+
---
+++ main/ssh.c
@@ -0,0 +1,225 @@
+/* ssh Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+
+#include "libssh2_config.h"
+#include <libssh2.h>
+#include <libssh2_sftp.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+static const char *TAG = "SSH";
+
+extern EventGroupHandle_t xEventGroup;
+extern int TASK_FINISH_BIT;
+
+static int waitsocket(int socket_fd, LIBSSH2_SESSION *session)
+{
+	struct timeval timeout;
+	int rc;
+	fd_set fd;
+	fd_set *writefd = NULL;
+	fd_set *readfd = NULL;
+	int dir;
+
+	timeout.tv_sec = 10;
+	timeout.tv_usec = 0;
+
+	FD_ZERO(&fd);
+
+	FD_SET(socket_fd, &fd);
+
+	/* now make sure we wait in the correct direction */
+	dir = libssh2_session_block_directions(session);
+
+	if(dir & LIBSSH2_SESSION_BLOCK_INBOUND)
+		readfd = &fd;
+
+	if(dir & LIBSSH2_SESSION_BLOCK_OUTBOUND)
+		writefd = &fd;
+
+	rc = select(socket_fd + 1, readfd, writefd, NULL, &timeout);
+
+	return rc;
+}
+
+#define BUFSIZE 3200
+
+void ssh_task(void *pvParameters)
+{
+    char *task_parameter = (char *)pvParameters;
+    ESP_LOGI(pcTaskGetTaskName(0), "Start task_parameter=%s", task_parameter);
+
+	// SSH Staff
+	int sock;
+	struct sockaddr_in sin;
+	LIBSSH2_SESSION *session;
+	LIBSSH2_CHANNEL *channel;
+
+	ESP_LOGI(TAG, "libssh2_version is %s", LIBSSH2_VERSION);
+	int rc = libssh2_init(0);
+	if(rc) {
+		ESP_LOGE(TAG, "libssh2 initialization failed (%d)", rc);
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* Ultra basic "connect to port 22 on localhost"
+	 * Your code is responsible for creating the socket establishing the
+	 * connection
+	 */
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(-1 == sock) {
+		ESP_LOGE(TAG, "failed to create socket!");
+		while(1) { vTaskDelay(1); }
+	}
+
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(22);
+	sin.sin_addr.s_addr = inet_addr(CONFIG_SSH_HOST);
+	if(connect(sock, (struct sockaddr*)(&sin),
+			   sizeof(struct sockaddr_in)) != 0) {
+		ESP_LOGE(TAG, "failed to connect!");
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* Create a session instance
+	 */
+	session = libssh2_session_init();
+	if(!session) {
+		ESP_LOGE(TAG, "failed to session init");
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* ... start it up. This will trade welcome banners, exchange keys,
+	 * and setup crypto, compression, and MAC layers
+	 */
+	rc = libssh2_session_handshake(session, sock);
+	if(rc) {
+		ESP_LOGE(TAG, "Failure establishing SSH session: %d", rc);
+		while(1) { vTaskDelay(1); }
+	}
+
+	/* We could authenticate via password */
+	if(libssh2_userauth_password(session, CONFIG_SSH_USER, CONFIG_SSH_PASSWORD)) {
+		ESP_LOGE(TAG, "Authentication by password failed.");
+		ESP_LOGE(TAG, "Authentication username : [%s].", CONFIG_SSH_USER);
+		while(1) { vTaskDelay(1); }
+	}
+
+
+	libssh2_trace(session, LIBSSH2_TRACE_SOCKET);
+
+	/* Exec non-blocking on the remove host */
+	while((channel = libssh2_channel_open_session(session)) == NULL &&
+		  libssh2_session_last_error(session, NULL, NULL, 0) ==
+		  LIBSSH2_ERROR_EAGAIN) {
+		waitsocket(sock, session);
+	}
+	if(channel == NULL) {
+		ESP_LOGE(TAG, "libssh2_channel_open_session failed.");
+		while(1) { vTaskDelay(1); }
+	}
+
+	while((rc = libssh2_channel_exec(channel, task_parameter)) ==
+		  LIBSSH2_ERROR_EAGAIN)
+		waitsocket(sock, session);
+
+	if(rc != 0) {
+		ESP_LOGE(TAG, "libssh2_channel_exec failed: %d", rc);
+		while(1) { vTaskDelay(1); }
+	}
+
+	int bytecount = 0;
+	for(;;) {
+		/* loop until we block */
+		int rc;
+		do {
+			char buffer[128];
+			rc = libssh2_channel_read(channel, buffer, sizeof(buffer) );
+			if(rc > 0) {
+				int i;
+				bytecount += rc;
+				//fprintf(stderr, "We read:\n");
+				for(i = 0; i < rc; ++i)
+					//fputc(buffer[i], stderr);
+					fputc(buffer[i], stdout);
+				//fprintf(stderr, "\n");
+			}
+			else if(rc < 0) {
+					/* no need to output this for the EAGAIN case */
+					ESP_LOGI(TAG, "libssh2_channel_read returned %d", rc);
+					//while(1) { vTaskDelay(1); }
+			}
+		}
+		while(rc > 0);
+
+		/* this is due to blocking that would occur otherwise so we loop on
+		   this condition */
+		if(rc == LIBSSH2_ERROR_EAGAIN) {
+			waitsocket(sock, session);
+		}
+		else
+			break;
+	}
+	printf("\n");
+
+
+	int exitcode = 127;
+	char *exitsignal = (char *)"none";
+	while((rc = libssh2_channel_close(channel)) == LIBSSH2_ERROR_EAGAIN)
+		waitsocket(sock, session);
+	if(rc == 0) {
+		exitcode = libssh2_channel_get_exit_status(channel);
+		libssh2_channel_get_exit_signal(channel, &exitsignal,
+										NULL, NULL, NULL, NULL, NULL);
+	} else {
+		ESP_LOGE(TAG, "libssh2_channel_close failed: %d", rc);
+		while(1) { vTaskDelay(1); }
+	}
+
+	if(exitsignal)
+		ESP_LOGI(TAG, "EXIT: %d, SIGNAL: %s, bytecount: %d", exitcode, exitsignal, bytecount);
+		//ESP_LOGI(TAG, "Got signal: %s", exitsignal);
+	else
+		ESP_LOGI(TAG, "EXIT: %d, bytecount: %d", exitcode, bytecount);
+
+	libssh2_channel_free(channel);
+	channel = NULL;
+
+
+	// Close a session
+	libssh2_session_disconnect(session,
+							   "Normal Shutdown, Thank you for playing");
+	libssh2_session_free(session);
+
+
+	// Close socket
+	close(sock);
+	ESP_LOGI(TAG, "[%s] done", task_parameter);
+
+	libssh2_exit();
+
+	xEventGroupSetBits( xEventGroup, TASK_FINISH_BIT );
+	vTaskDelete( NULL );
+
+}
